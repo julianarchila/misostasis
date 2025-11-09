733bbfe114aa477df3b85e2ffabf17c8e2fac5ea
Julian Archila <julianarchilacaro@gmail.com>
2025-11-09 12:09:29 -0500

    minor

--- Proyecto/lieou/src/app/sign-in/[[...index]]/page.tsx
+++ Proyecto/lieou/src/app/sign-in/[[...index]]/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   return (
     <div className="min-h-[100svh] bg-white flex items-center justify-center p-4">
       <div className="w-full max-w-sm">
-        <SignIn routing="path" />
+        <SignIn />
       </div>
     </div>
   );
